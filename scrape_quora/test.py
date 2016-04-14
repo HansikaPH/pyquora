24975fae0efbd897405c2371943744911558e6dc
hansika <pehesarani@gmail.com>
2016-04-14 12:21:57 +0530

    package updated

--- scrape_quora/test.py
+++ scrape_quora/test.py
@@ -17,3 +17,21 @@ print Scrape_Quora.get_no_of_followers(user_name)
 print Scrape_Quora.get_no_of_following(user_name)
 
 print Scrape_Quora.get_no_of_edits(user_name)
+
+user_name = 'Hansika-Hewamalage'
+
+print Scrape_Quora.get_name(user_name)
+
+print Scrape_Quora.get_url(user_name)
+
+print Scrape_Quora.get_profile_picture_link(user_name)
+
+print Scrape_Quora.get_no_of_questions(user_name)
+
+print Scrape_Quora.get_no_of_answers(user_name)
+
+print Scrape_Quora.get_no_of_followers(user_name)
+
+print Scrape_Quora.get_no_of_following(user_name)
+
+print Scrape_Quora.get_no_of_edits(user_name)
